5e386bde3a31aa8bde87b70af97473e4f0ccef2e
Vera Deeva <verasdee@gmail.com>
2023-12-15 13:50:00 +0700

    feat: chane html, add css files and js files

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,206 +1,279 @@
 const cityOpen = document.querySelector('.js-city-open');
 const city = document.querySelector('.city');
 
-const headerBurger = $('.header__burger');
-const navigation = $('.navigation');
-const navigationClose = $('.navigation__close');
-const presentBtn = $('.present__btn');
-const modalOrder = $('.modal-order')
-const modalOrderClose = $('.modal-order__close');
-const headerSign = $('.header__sign2');
-const alert = $('.alert');
+cityOpen.addEventListener('click', () => {
+  city.classList.add('city_open')
+});
+
+city.addEventListener('click', (e) => {
+  e.preventDefault();
+  const target = e.target.closest('.city__choice');
+  if (target) {
+    cityOpen.textContent = target.textContent;
+    city.classList.remove('city_open');
+  }
+});
+
+
+$('.header__burger').on('click', function () {
+
+  $('.navigation').animate({
+    left: 0,
+  }, 500, function () {
+    $('.navigation__close').animate({
+      opacity: 1,
+    }, 300);
+  });
+});
+
+$('.navigation__close').on('click', function () {
+  $('.navigation__close').animate({
+    opacity: 0,
+  }, 300, function () {
+    $('.navigation').animate({
+      left: '-400px',
+    }, 500);
+  });
+});
+
+
+$('.present__btn').on('click', function () {
+  $('.modal-order').show(300);
+})
+
+$('.modal-order__close').on('click', function () {
+  $('.modal-order').hide(300);
+})
+
+
+$('.header__sign, .header__sign2').click(() => {
+  $('.alert').attr("role", "alert");
+
+  $('.alert').addClass('visible');
+  setTimeout(() => {
+    $('.alert').removeClass('visible');
+
+    $('.alert').removeAttr("role", "alert");
+
+  }, 3000)
+})
+
 const characteristicsListElem = document.querySelector('.characteristics__list');
 const characteristicsItemElems = document.querySelectorAll('.characteristics__item');
 
-//!--innert 
-
-let prevActiveElement;
-
-
-function addInnert(elem) {
-    prevActiveElement = document.activeElement;
-    for (let i = 0; i < document.body.children.length; i++) {
-        if (document.body.children[i] !== elem) {
-            document.body.children[i].inert = true;
-        }
-    };
-    elem.inert = false;
-    if (elem.closest('[inert]')) elem.closest('[inert]').inert = false;
-
-    for (let i = 0; i < window.elemsInert.length; i++) {
-        if (elem === window.elemsInert[i].elem && window.elemsInert[i].esc) {
-            function esc(e) {
-                if (e.key == 'Escape') {
-                    window.elemsInert[i].esc()
-                }
-                document.removeEventListener('keydown', esc);
-            }
-            document.addEventListener('keydown', esc);
-        }
-
-    }
-}
-
-function activationInnert(elemsInert) {
-    window.elemsInert = elemsInert;
-
-    function removeInnert(elem) {
-        if (elem) {
-            for (let i = 0; i < document.body.children.length; i++) {
-                if (document.body.children[i] !== elem) {
-                    document.body.children[i].inert = false;
-                }
-            };
-            elem.innert = true;
-            prevActiveElement.focus();
-        }
-
-        for (let i = 0; i < elemsInert.length; i++) {
-            if (window.screen.width <= elemsInert[i].breakpoints || !elemsInert[i].breakpoints) {
-                elemsInert[i].elem.inert = true
-                console.log(elemsInert[i]);
-            }
-        }
-    }
-    removeInnert();
-
-    return removeInnert
-}
-
-//!--end-innert
-
-const removeInnert = activationInnert([
-    {
-        elem: city,
-        esc: choiseCity
-    },
-    {
-        elem: modalOrder.get(0),
-        esc: closeModal
-    },
-    {
-        elem: navigation.get(0),
-        esc: closeBurger,
-        breakpoints: 1000
-    }
-]);
-
-function openChoiseCity() {
-    city.classList.add('city_open')
-    addInnert(city);//!--активируем inert на city
-}
-
-function choiseCity(event) {
-    if (!event) { // тригер закрытия
-        city.classList.remove('city_open');
-        removeInnert(city) //!--Деактивируем inert 
-        return;
-    }
-    event.preventDefault();
-    const target = event.target.closest('.city__choice');
-    if (target) {
-        cityOpen.textContent = target.textContent;
-        city.classList.remove('city_open');
-        removeInnert(city) //!--Деактивируем inert 
-    }
-}
-
-cityOpen.addEventListener('click', openChoiseCity);
-city.addEventListener('click', choiseCity);
-
-
-function openBurger() {
-    navigation.addClass('navigation_open').animate({
-        left: 0,
-    }, 500, function () {
-        addInnert(navigation.get(0));//!--активируем inert на navigation
-        navigationClose.animate({
-            opacity: 1,
-        }, 300);
-    });
-}
-
-function closeBurger() {
-    navigationClose.animate({
-        opacity: 0,
-    }, 300, function () {
-        removeInnert(navigation.get(0))//!--Деактивируем inert 
-        navigation.animate({
-            left: '-400px',
-        }, 500).removeClass('navigation_open');
-    });
-}
-
-headerBurger.on('click', openBurger)
-
-navigationClose.on('click', closeBurger);
-
-function openModal() {
-    modalOrder.show(300);
-    addInnert(modalOrder.get(0));//!--активируем inert на modalOrder
-}
-
-function closeModal() {
-    modalOrder.hide(300);
-    removeInnert(modalOrder.get(0))//!--Деактивируем inert 
-}
-
-presentBtn.on('click', openModal)
-
-modalOrderClose.on('click', closeModal)
-
-const openAlert = () => {
-    alert.attr("role", "alert");
-
-    alert.addClass('visible');
-    setTimeout(() => {
-        alert.removeClass('visible');
-
-        alert.removeAttr("role", "alert");
-
-    }, 3000)
-};
-
-headerSign.click(openAlert)
-
 characteristicsItemElems.forEach(elem => {
-    if (elem.children[1].classList.contains('active')) {
-        elem.children[1].style.height = elem.children[1].scrollHeight + 'px';
-    }
+  if (elem.children[1].classList.contains('active')) {
+    elem.children[1].style.height = `${elem.children[1].scrollHeight}px`;
+  }
 })
 
 const open = (button, dropDown) => {
-    closeAllDrops(button, dropDown);
-    button.ariaExpanded = true;
+  closeAllDrops(button, dropDown);
+  button.ariaExpanded = true;
 
-    dropDown.style.height = dropDown.scrollHeight + 'px';
-    button.classList.add('active');
-    dropDown.classList.add('active');
+  dropDown.style.height = `${dropDown.scrollHeight}px`;
+  button.classList.add('active');
+  dropDown.classList.add('active');
 };
 
 const close = (button, dropDown) => {
-    button.ariaExpanded = false;
-    button.classList.remove('active');
-    dropDown.classList.remove('active');
-    dropDown.style.height = '';
+  button.ariaExpanded = false;
+  button.classList.remove('active');
+  dropDown.classList.remove('active');
+  dropDown.style.height = '';
 };
 
 const closeAllDrops = (button, dropDown) => {
-    characteristicsItemElems.forEach((elem) => {
-        if (elem.children[0] !== button && elem.children[1] !== dropDown) {
-
-            close(elem.children[0], elem.children[1]);
-        }
-    })
-}
+  characteristicsItemElems.forEach((elem) => {
+    if (elem.children[0] !== button && elem.children[1] !== dropDown) {
+      close(elem.children[0], elem.children[1]);
+    }
+  });
+};
 
 characteristicsListElem.addEventListener('click', (event) => {
-    const target = event.target;
-    if (target.classList.contains('characteristics__title')) {
-        const parent = target.closest('.characteristics__item');
-        const description = parent.querySelector('.characteristics__description');
-        description.classList.contains('active') ?
-            close(target, description) :
-            open(target, description);
-    }
+  const target = event.target;
+  if (target.classList.contains('characteristics__title')) {
+    const parent = target.closest('.characteristics__item');
+    const description = parent.querySelector('.characteristics__description');
+    description.classList.contains('active')
+      ? close(target, description)
+      : open(target, description);
+  }
 });
+
+const modalOrderFielfset = document.querySelector('.modal-order__fielfset');
+const modalOrderTitle = document.querySelector('.modal-order__title');
+const cookieAlert = document.querySelector('.alert-cookie');
+const cookieButton = document.querySelector('.alert-cookie__button');
+
+cookieButton.addEventListener('click', () => {
+  cookieAlert.classList.remove('alert-cookie_no-ready');
+  Cookies.set('dom-ready-cookie', 'true', {
+    expires: 10,
+  })
+});
+
+if (!Cookies.get('dom-ready-cookie', 'true')) {
+  cookieAlert.classList.add('alert-cookie_no-ready');
+};
+
+const inputTel = document.querySelector('.modal-order__input_tel');
+const telMask = new Inputmask('+7 (999)-999-99-99');
+telMask.mask(inputTel);
+
+const justValidate = new JustValidate('.modal-order__form');
+
+justValidate
+  .addField('.modal-order__input', [
+    {
+      rule: 'required',
+      errorMessage: 'Укажите ваше имя',
+    },
+    {
+      rule: 'minLength',
+      value: 3,
+      errorMessage: 'Не короче 3-х символов',
+    },
+    {
+      rule: 'maxLength',
+      value: 30,
+      errorMessage: 'Не длинее 30-и символов',
+    },
+  ])
+  .addField('.modal-order__input_email', [
+    {
+      rule: 'required',
+      errorMessage: 'Укажите ваш email',
+    },
+    {
+      rule: 'email',
+      errorMessage: 'email не корректный!',
+    },
+  ])
+  .addField('.modal-order__input_tel', [
+    {
+      rule: 'required',
+      errorMessage: 'Укажите ваш телефон',
+    },
+    {
+      validator(value) {
+        const phone = inputTel.inputmask.unmaskedvalue();
+        console.log(phone);
+        return !!(Number(phone) && phone.length === 10);
+      },
+      errorMessage: 'Телефон не корректный!'
+    },
+  ])
+  .onSuccess(eve => {
+    const target = eve.target;
+    axios.post('https://jsonplaceholder.typicode.com/posts', {
+      name: target.name.value,
+      tel: inputTel.inputmask.unmaskedvalue(),
+      // tel: target.tel.value,
+      email: target.email.value,
+    })
+      .then(response => {
+        target.reset();
+        modalOrderFielfset.disabled = true;
+        modalOrderTitle.textContent = `Спасибо, ваша заявка принята. Номер заявки ${response.data.id}`
+      })
+      .catch(err => {
+        console.error(err);
+        modalOrderFielfset.disabled = false;
+        modalOrderTitle.textContent = `Что-то пошло не так, попробуйте позже`
+      });
+  });
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  slidesPerView: 3,
+  loop: true,
+  // autoplay: {
+  //   delay: 2000,
+  // },
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    // nextEl: '.swiper-button-next',
+    // prevEl: '.swiper-button-prev',
+    nextEl: '.slider__button-right',
+    prevEl: '.slider__button-left',
+  },
+  mousewheel: true,
+  keyboard: true,
+  // And if we need scrollbar
+  // scrollbar: {
+  //   el: '.swiper-scrollbar',
+  // },
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      direction: 'vertical',
+      slidesPerView: 1,
+      spaceBetween: 5
+    },
+    // when window width is >= 480px
+    480: {
+      direction: 'horizontal',
+      slidesPerView: 2,
+      spaceBetween: 10
+    },
+    // when window width is >= 640px
+    678: {
+      slidesPerView: 3,
+      spaceBetween: 20
+    }
+  }
+});
+
+$('.acc__list').accordion({
+  active: true,
+  collapsible: true,
+  heightStyle: 'content',
+  icons: {
+    header: 'acc__accord',
+    activeHeader: 'acc__accord acc__accord-active',
+  }
+});
+
+ymaps.ready(init);
+function init() {
+  const myMap = new ymaps.Map("map_kant", {
+    center: [54.706259, 20.511145],
+    zoom: 17
+  });
+
+  const mark = new ymaps.Placemark([54.706259, 20.511145], {}, {
+    iconLayout: 'default#image',
+    iconImageHref: 'img/pin.svg',
+    iconImageSize: [60, 60],
+    iconImageOffset: [-25, -60],
+  });
+  const bridge = new ymaps.Placemark([54.706006, 20.513785], {
+    hintContent: 'Медовый мост',
+    balloonContent: '<div class="acc__title">Середина моста</div>'
+  }, {
+    iconLayout: 'default#image',
+    iconImageHref: 'img/pin.svg',
+    iconImageSize: [50, 50],
+    iconImageOffset: [-25, -10],
+  });
+
+  myMap.geoObjects.add(mark).add(bridge);
+
+  /* myMap.behaviors.disable('scrollZoom'); // запрещает зум колёсиком мыши
+   myMap.behaviors.disable('drag'); // запрещает скролл свайпом
+   myMap.controls.remove('geolocationControl'); // удаляет геолокацию (мое местоположение)
+   myMap.controls.remove('searchControl'); //  удаляет поиск
+   myMap.controls.remove('trafficControl'); //  удаляет контроль трафика
+   myMap.controls.remove('typeSelector'); //  удаляет тип
+   myMap.controls.remove('fullscreenControl'); // удаляет переход в полноэкранный режим
+   myMap.controls.remove('zoomControl'); // удаляет контроль зума
+   myMap.controls.remove('rulerControl'); // удаляет контроль правил
+   */
+}
